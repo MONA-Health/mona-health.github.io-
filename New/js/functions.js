11c78d7d2894d8ea8076eccd7bd92c195e139522
StefRommes <stef.rommes@vito.be>
2021-04-28 14:54:43 +0200

    Both eyes

--- New/js/functions.js
+++ New/js/functions.js
@@ -1,8 +1,19 @@
-async function loadFile() {
+async function loadFileLeft() {
 	console.log("image is in loadfile..");
   	var fileInputElement = document.getElementById("select-file-image");
   	console.log(fileInputElement.files[0]);
-    renderImage(fileInputElement.files[0]);
+    renderImage(fileInputElement.files[0], 'test-image-1');
+    showPredBtn();
+	showImgDiv();
+	//hideUploadAndReq();
+	//changeDotToGreen();
+}
+
+async function loadFileRight() {
+	console.log("image is in loadfile..");
+	var fileInputElement = document.getElementById("select-file-image-2");
+  	console.log(fileInputElement.files[0]);
+    renderImage(fileInputElement.files[0], 'test-image-2');
     showPredBtn();
 	showImgDiv();
 	hideUploadAndReq();
@@ -23,13 +34,13 @@ function hideUploadAndReq() {
 	document.getElementById("reqDiv").style.display = "none";
 }
 
-function renderImage(file) {
+function renderImage(file, id) {
   var reader = new FileReader();
   console.log("image is here..");
   reader.onload = function(event) {
     img_url = event.target.result;
     console.log("image is here2..");
-    document.getElementById("test-image").src = img_url;
+    document.getElementById(id).src = img_url;
   }
   reader.readAsDataURL(file);
 }
@@ -38,27 +49,52 @@ function showPredBtn() {
     document.getElementById("predBtn").style.display = "flex";
 }
 
-function showResult(data) {
+function showResults(dataL, dataR) {
+	showResultL(dataL);
+	showResultR(dataR);
+}
+
+function showResultL(data) {
     document.getElementById("resultTxt").style.display = "flex";
-	document.getElementById("slider").style.display = "flex";
+	document.getElementById("sliderL").style.display = "flex";
+	document.getElementById("resetBtn").style.display = "flex";
 	var prediction = Math.round( data['prediction'] * 10 + Number.EPSILON ) / 10;
-	document.getElementById("sliderTxt").innerHTML = prediction.toString();
+	document.getElementById("sliderTxtL").innerHTML = prediction.toString();
 	var pos = prediction/4*95;
-	document.getElementById("sliderControl").style.left = pos.toString() + "%";
+	document.getElementById("sliderControlL").style.left = pos.toString() + "%";
 	var unrounded = Math.round( data['prediction'] * 10000 + Number.EPSILON ) / 10000;
+	console.log('L:' + unrounded.toString());
 	document.getElementById("fullResult").innerHTML = unrounded.toString();
 }
 
-function predBtn() {
-    let image  = document.getElementById("test-image");
-    console.log('xxxx')
-    console.log(image)
+function showResultR(data) {
+	document.getElementById("sliderR").style.display = "flex";
+	var prediction = Math.round( data['prediction'] * 10 + Number.EPSILON ) / 10;
+	document.getElementById("sliderTxtR").innerHTML = prediction.toString();
+	var pos = prediction/4*95;
+	document.getElementById("sliderControlR").style.left = pos.toString() + "%";
+	var unrounded = Math.round( data['prediction'] * 10000 + Number.EPSILON ) / 10000;
+	console.log('L:' + unrounded.toString());
+	document.getElementById("fullResult").innerHTML = unrounded.toString();
+}
+
+async function predBtn() {
+    let image  = document.getElementById("test-image-1");
+	let image2  = document.getElementById("test-image-2");
+    //console.log('xxxx')
+    //console.log(image)
 
     let _data = {
         input: [image.src]
     }
+	
+	let _data2 = {
+        input: [image2.src]
+    }
     	
-    postData(_data).then(data => showResult(data))
+	let [data1, data2] = await Promise.all([postData(_data), postData(_data2)]);	
+	
+	showResults(data1, data2)
 
 //     fetch('https://doynj7ndmjy4ibntttu3zuhcji.apigateway.eu-frankfurt-1.oci.customer-oci.com/demo/pred', {
 //             method: "POST",
